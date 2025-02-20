07e2a7052293355a15127866d91d89c8e1f247a4
ameyjoshi3068 <ameyjoshi3068@gmail.com>
2025-02-20 17:21:36 +0530

    Added Backend

--- spam_call_shield/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import CallRecording
-
-class CallRecordingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CallRecording
-        fields = '__all__'
--- spam_call_shield/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import UploadCallRecording
-
-urlpatterns = [
-    path('upload/', UploadCallRecording.as_view(), name='upload-call'),
-]
